a05190292db75c7dc456ed63e5e9df589b43b1a0
Patrick Rallings <patrickrallings@Patricks-MBP.localdomain>
2022-04-18 14:04:02 -0400

    Changed color scheme to be darker

--- 1.12-1.14/src/components/MostPopular.js
+++ 1.12-1.14/src/components/MostPopular.js
@@ -18,8 +18,8 @@ export default function MostPopular({top, value}) {
     <h2>Most Popular Anecdote</h2>
     <h5 className='col mt-2 votes'><i>(with {value} votes)</i></h5>
     <hr className='votesHR' />
-        <div className='col mt-2'>
-        <h4>"{top}"</h4>
+        <div className='col mt-4'>
+        <h2>"{top}"</h2>
         </div>
     </>
   )
